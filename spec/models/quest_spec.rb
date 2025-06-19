3091c19ecb44bbcd6f2ecefc01487d8d5515b574
Kitikun <kitikun.po@gmail.com>
2025-06-19 10:52:01 +0700

    feat: generate Quest model with title and status

---
+++ spec/models/quest_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Quest, type: :model do
+  describe 'validations' do
+    it 'is valid' do
+      quest = Quest.new(title: 'Git 101', status: false)
+      expect(quest).to be_valid
+    end
+
+    it 'is not valid' do
+      quest = Quest.new(title: nil, status: false)
+      expect(quest).to_not be_valid
+    end
+  end
+end
