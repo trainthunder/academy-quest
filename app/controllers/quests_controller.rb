b4f27cf5d0327f9db9b6a6305ef1d454cf627f4a
Kitikun <kitikun.po@gmail.com>
2025-06-19 13:34:08 +0700

    feat: form to create a new quest and render all quest order by create date

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,24 @@
 class QuestsController < ApplicationController
   def index
+    @quests = Quest.order(created_at: :desc)
+    @quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    if @quest.save
+      respond_to do |format|
+        format.turbo_stream
+        # format.html { redirect_to quests_path }
+        redirect_to quests_path
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+  def quest_params
+    params.require(:quest).permit(:title, :status)
   end
 end
--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe "Quests", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+  describe "POST /quests" do
+    it "can create a new quest" do
+      expect {
+        post quests_path,
+        params: {
+          quest: {
+            title: "Git 101",
+            status: false
+            }
+          }
+      }.to change(Quest, :count).by(1)
+    end
+  end
 end
